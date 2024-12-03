10d3f99cfc6dba605e53a797d29ea37e246a6b00
Bridger Tower <bridgertower@gmail.com>
2024-12-03 15:01:10 -0700

    wip

--- app/(site)/docs/[id]/renderer.tsx
+++ app/(site)/docs/[id]/renderer.tsx
@@ -3,7 +3,7 @@ import { NotionRenderer } from "react-notion-x";
 import type { ComponentProps } from "react";
 
 const mapPageUrl = (pageId: string) => {
-  return `/docs/${pageId.replace(/-/g, '')}`;
+  return `/docs/${pageId}`;
 };
 
 export function Renderer(props: ComponentProps<typeof NotionRenderer>) {
