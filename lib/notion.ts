62dfb0d26183e9c483d45876f8ea1fb0657af631
Bridger Tower <bridgertower@gmail.com>
2024-12-03 14:32:10 -0700

    wip

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,37 +1,40 @@
-import {Client} from "@notionhq/client"
-import {PageObjectResponse, RichTextItemResponse} from "@notionhq/client/build/src/api-endpoints";
-import {NotionCompatAPI} from "notion-compat";
+import { Client } from "@notionhq/client";
+import {
+  PageObjectResponse,
+  RichTextItemResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import { NotionCompatAPI } from "notion-compat";
 
-const apiKey = process.env.NOTION_API_KEY
+const apiKey = process.env.NOTION_API_KEY;
 
-export const notion = new Client({auth: apiKey})
+export const notion = new Client({ auth: apiKey });
 
 /**
  * Non-official client used for react-notion-x
  */
-export const notionCustom = new NotionCompatAPI(notion)
+export const notionCustom = new NotionCompatAPI(notion);
 
 export interface PageInfo {
-    title: string
+  title: string;
 }
 
 export function getPageInfo(page: PageObjectResponse): PageInfo {
-    const info: PageInfo = {title: 'unknown'}
-    const keys = Object.keys(page.properties)
+  const info: PageInfo = { title: "unknown" };
+  const keys = Object.keys(page.properties);
 
-    for (const key of keys) {
-        if (page.properties[key].type === 'title') {
-            info.title = getPlainText(page.properties[key].title)
-        }
+  for (const key of keys) {
+    if (page.properties[key].type === "title") {
+      info.title = getPlainText(page.properties[key].title);
     }
+  }
 
-    return info
+  return info;
 }
 
 export function getPlainText(rich: RichTextItemResponse[]): string {
-    return rich.map(v => v.plain_text).join()
+  return rich.map((v) => v.plain_text).join();
 }
 
 export function getPageUrl(page: PageObjectResponse) {
-    return `/docs/${page.id}`
+  return `/docs/${page.id}`;
 }
