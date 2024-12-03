fd7c2fa776486424916739ec52112e12fb5c8072
Bridger Tower <bridgertower@gmail.com>
2024-12-03 15:00:15 -0700

    wip

--- app/(site)/docs/[id]/renderer.tsx
+++ app/(site)/docs/[id]/renderer.tsx
@@ -2,6 +2,10 @@
 import { NotionRenderer } from "react-notion-x";
 import type { ComponentProps } from "react";
 
+const mapPageUrl = (pageId: string) => {
+  return `/docs/${pageId.replace(/-/g, '')}`;
+};
+
 export function Renderer(props: ComponentProps<typeof NotionRenderer>) {
-  return <NotionRenderer {...props} />;
+  return <NotionRenderer {...props} mapPageUrl={mapPageUrl} />;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Manrope as Font } from "next/font/google";
 import type { ReactNode } from "react";
 
+export const metadata = {
+  title: "Notes by Bridger Tower / Designer and Developer",
+  description:
+    "This site is a collection of notes and ideas about design and development created by Bridger Tower.",
+};
+
 const font = Font({
   subsets: ["latin"],
 });
