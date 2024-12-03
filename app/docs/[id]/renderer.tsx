17a6b35314aecab01e9cb6eb510b4066d7a1898d
Bridger Tower <bridgertower@gmail.com>
2024-12-03 14:41:03 -0700

    wip

--- app/docs/[id]/renderer.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import {NotionRenderer} from "react-notion-x";
-import type {ComponentProps} from "react";
-
-export function Renderer(props: ComponentProps<typeof NotionRenderer>) {
-    return <NotionRenderer {...props} />
-}
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,7 +1,7 @@
-import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export const baseOptions: BaseLayoutProps = {
-    nav: {
-        title: 'My Notion',
-    },
+  nav: {
+    title: "Docs / Bridger Tower",
+  },
 };
