6edfdf7bfe8448bdc98ea06b8926122c0721c39c
Bridger Tower <bridgertower@gmail.com>
2024-12-03 14:44:35 -0700

    wip

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,3 @@
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { defineConfig } from "fumadocs-mdx/config";
 
-export const { docs, meta } = defineDocs({
-  dir: "content/docs",
-});
-
-export default defineConfig();
+export default defineConfig({});
