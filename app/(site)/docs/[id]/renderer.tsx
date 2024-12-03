2e27bab68fc5ac74560cddbf66ca274a41cda1bf
Bridger Tower <bridgertower@gmail.com>
2024-12-03 15:06:11 -0700

    wip

--- app/(site)/docs/[id]/renderer.tsx
+++ app/(site)/docs/[id]/renderer.tsx
@@ -1,11 +1,25 @@
 "use client";
 import { NotionRenderer } from "react-notion-x";
 import type { ComponentProps } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the code component
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then((m) => m.Code)
+);
 
 const mapPageUrl = (pageId: string) => {
   return `/docs/${pageId}`;
 };
 
 export function Renderer(props: ComponentProps<typeof NotionRenderer>) {
-  return <NotionRenderer {...props} mapPageUrl={mapPageUrl} />;
+  return (
+    <NotionRenderer
+      {...props}
+      mapPageUrl={mapPageUrl}
+      components={{
+        Code: Code,
+      }}
+    />
+  );
 }
